53d75bcabcf7a8e2aba2301285ecdccb6f5ce40f
Aberche <73166328+Aberche@users.noreply.github.com>
2024-02-15 21:06:43 +0100

    BasicControlls

--- Assets/S_Scrips/S_Character/S_CMovement.cs
+++ Assets/S_Scrips/S_Character/S_CMovement.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class S_CMovement : MonoBehaviour
 {
     float speed;
-    float walkSpeed = 7;
-    float sprintSpeed = 15;
-    float walkAcceleration = 75;
-    float airAcceleration = 50;
-    float groundDeceleration = 200;
-    float jumpHeight = 12;
-    public float Cgravity;
-    public float mayJump = 1;
-    public bool alreadyjumped;
+    float walkSpeed = 40;
+    float sprintSpeed = 80;
 
-    private CapsuleCollider2D capCollider;
+    float maxSpeed;
+    float maxWalkSpeed = 5;
+    float maxSprintSpeed = 10;
+    float groundDeceleration = 100;
+
+    public float direction;    
+    private bool changedirection => (rB.velocity.x > 0f && direction < 0) ||  (rB.velocity.x < 0f && direction > 0);
+
+    float jumpHeight = 20;
+    public float fallGravityM = 1;
+    public float limitJumpTime = 10;
+    public float mayJump = 1;
+    public bool isJumping;
+    public bool alreadyJumped;
+    
+    private bool onGround;
+
+    public bool enableGroundCheck = true; 
+
+
+    private Rigidbody2D rB;
 
     private Vector2 velocity;
-
-    public Vector2 collBox;
+    public Vector2 boxSize;
     public float castDistance;
     public LayerMask groundLayer; 
 
@@ -27,55 +39,36 @@ public class S_CMovement : MonoBehaviour
 
     private void Awake()
     {      
-        capCollider = GetComponent<CapsuleCollider2D>();
+        rB = GetComponent<Rigidbody2D>();
+    }
+
+    public Vector2 GetInput()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+        ApplyDeacceleration();
+        Debug.Log(onGround);
     }
 
     private void Update()
     {
-        speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;
-        float moveInput = Input.GetAxisRaw("Horizontal");
+        Animations();
+        JumpHability();
+        onGround = Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer);
+    }
 
-        float acceleration = isGrounded() ? walkAcceleration : airAcceleration;
-        float deceleration = isGrounded() ? groundDeceleration : 0;
-
-        //Jump
-        if (isGrounded())
+    public void Animations()
+    {
+        //Basic Animations
+        if (direction != 0)
         {
-            velocity.y = 0;
-            mayJump = 1;
-        }
-        else
-        {
-            mayJump -= Time.deltaTime;
-            Cgravity = velocity.y;
-            velocity.y += (Physics2D.gravity.y * 2) * Time.deltaTime;
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            if(isGrounded())
-            {
-                velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
-                
-            }
-            else if(alreadyjumped == false && mayJump >=0)
-            {
-                velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
-            }
-            else
-            {
-                alreadyjumped = true;
-            }
-            
-        }
-
-        //Basic Movement
-        if (moveInput != 0)
-        {
-            velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInput, acceleration * Time.deltaTime);
             animatorCharacter.SetBool("Static",false);
             animatorCharacter.SetBool("Walk",true);
-            if(velocity.x < 0)
+            if(direction < 0)
             {
                 gameObject.GetComponent<SpriteRenderer>().flipX = true;
             }
@@ -88,27 +81,84 @@ public class S_CMovement : MonoBehaviour
         {
             animatorCharacter.SetBool("Walk",false);
             animatorCharacter.SetBool("Static",true);
-            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
-        }
-
-        transform.Translate(velocity * Time.deltaTime);
-
+        }  
     }
 
-    public bool isGrounded()
-    {
-        if(Physics2D.BoxCast(transform.position, collBox, 0, -transform.up, castDistance, groundLayer))
+    private void MoveCharacter(){
+        if(onGround)
         {
-            return true;
+            speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;            
+            maxSpeed = Input.GetKey(KeyCode.LeftShift) ? maxSprintSpeed :maxWalkSpeed;            
+        }
+
+        direction = GetInput().x;
+
+        rB.AddForce(new Vector2(direction, 0) * speed);
+
+        if(Mathf.Abs(rB.velocity.x) > maxSpeed)
+        {
+            rB.velocity = new Vector2(Mathf.Sign(rB.velocity.x) * maxSpeed, rB.velocity.y);
+        }
+    }
+
+    public void ApplyDeacceleration(){
+        if(Mathf.Abs(direction) < 0.2f && onGround && !isJumping)
+        {
+            rB.drag = groundDeceleration;
         }
         else
         {
-            return false;
+            rB.drag = 0;
+        }
+    }
+    
+    public void JumpHability()
+    {
+        if (Input.GetButtonDown("Jump")){
+            Jump();
+            fallGravityM = 1;
+        }
+
+        if(Input.GetButton("Jump"))
+        {
+            isJumping = true;
+            limitJumpTime -= 0.1f;
+            
+            if(limitJumpTime < 0)
+            {
+                isJumping = false;
+                rB.gravityScale = fallGravityM;
+                rB.drag = 0;
+            }
+        }
+
+        if(Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+            rB.gravityScale = fallGravityM;
+        }
+
+        if(onGround){
+            fallGravityM = 1;
+            rB.gravityScale = fallGravityM;
+            limitJumpTime = 10;
+        }
+        else if(!isJumping)
+        {
+            fallGravityM = 8;
+            rB.gravityScale = fallGravityM;
         }
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, collBox);
+    public void Jump(){
+        if(onGround)
+        {
+            rB.velocity = new Vector2(rB.velocity.x, 0);
+            rB.velocity = new Vector2(rB.velocity.x, jumpHeight);
+        }      
+    }
+
+    private void OnDrawGizmos(){
+        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, boxSize);
     }
 }
