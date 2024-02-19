08b96be5f9f52b37c90142319ad89e26faa7f0d5
Aberche <arnau.berche@enti.cat>
2024-02-19 13:20:21 +0100

    First itineration Buffer Time

--- Assets/S_Scrips/S_Character/S_CMovement.cs
+++ Assets/S_Scrips/S_Character/S_CMovement.cs
@@ -4,44 +4,59 @@ using UnityEngine;
 
 public class S_CMovement : MonoBehaviour
 {
+
+    //Movement
+     
+        //BaseSpeeds
     float speed;
     float walkSpeed = 40;
     float sprintSpeed = 80;
 
+        //Max Speeds
     float maxSpeed;
     float maxWalkSpeed = 7;
     float maxSprintSpeed = 14;
+        
+        //Deceleration
     float groundDeceleration = 100;
+
 
     public float direction;    
     private bool changedirection => (rB.velocity.x > 0f && direction < 0) ||  (rB.velocity.x < 0f && direction > 0);
 
-    float jumpHeight = 1200;
+    //Jump
+
+        //Base Height
+    private float jumpHeight = 1200;
+
+        //Fall Speed
     private float fallGravityAir = 5;
     private float fallGravityLand = 10;
 
-    public float limitJumpTime = 10;
-    public float mayJump = 1;
-    public bool isJumping;
-    public bool alreadyJumped;
-    
+        //Holding Jump
+    private float limitJumpTime = 10;
+
+        //Coyote
+    private bool isJumping;
+    private float coyoteCount = 0.5f;
+    private bool validCoyote;
+
+    //Input Buffer
+    private float bufferTime = 1;
+    public float bufferCounter;
+
+
+    //Groud Checks
     private bool onGround;
 
-    public bool enableGroundCheck = true; 
-
-
+    //Components
     private Rigidbody2D rB;
-
-    private Vector2 velocity;
-    public Vector2 boxSize;
-    public float castDistance;
-    public LayerMask groundLayer; 
-
-    public Animator animatorCharacter;
+    private Animator animatorCharacter;
 
     private void Awake()
     {      
         rB = GetComponent<Rigidbody2D>();
+        animatorCharacter = GetComponent<Animator>();
     }
 
     public Vector2 GetInput()
@@ -59,29 +74,7 @@ public class S_CMovement : MonoBehaviour
     {
         Animations();
         JumpHability();
-    }
-
-    public void Animations()
-    {
-        //Basic Animations
-        if (direction != 0)
-        {
-            animatorCharacter.SetBool("Static",false);
-            animatorCharacter.SetBool("Walk",true);
-            if(direction < 0)
-            {
-                gameObject.GetComponent<SpriteRenderer>().flipX = true;
-            }
-            else
-            {
-                gameObject.GetComponent<SpriteRenderer>().flipX = false;
-            }
-        }
-        else
-        {
-            animatorCharacter.SetBool("Walk",false);
-            animatorCharacter.SetBool("Static",true);
-        }  
+        Buffer();
     }
 
     private void MoveCharacter(){
@@ -100,7 +93,6 @@ public class S_CMovement : MonoBehaviour
             rB.velocity = new Vector2(Mathf.Sign(rB.velocity.x) * maxSpeed, rB.velocity.y);
         }
     }
-
     public void ApplyDeacceleration(){
         if(Mathf.Abs(direction) < 0.2f && onGround && !isJumping)
         {
@@ -111,31 +103,33 @@ public class S_CMovement : MonoBehaviour
             rB.drag = 0;
         }
     }
-    
     public void JumpHability()
     {
-        if (Input.GetButtonDown("Jump")){
+        if ((bufferCounter > 0f && (validCoyote || onGround))) 
+        {
             Jump();
+            bufferCounter = 0;
         }
 
-        if(Input.GetButton("Jump"))
+        if (Input.GetButton("Jump"))
         {
-            if(limitJumpTime > 0)
+            if (limitJumpTime > 0)
             {
                 isJumping = true;
-                limitJumpTime -= 10 * Time.deltaTime;            
+                limitJumpTime -= 10 * Time.deltaTime;
             }
             else
             {
-                Debugs();
+                coyoteCount = 0;
                 isJumping = false;
                 rB.gravityScale = fallGravityAir;
                 rB.drag = 0;
             }
         }
 
-        if(Input.GetButtonUp("Jump"))
+        if (Input.GetButtonUp("Jump"))
         {
+            coyoteCount = 0;
             isJumping = false;
             rB.gravityScale = fallGravityLand;
             limitJumpTime = 10;
@@ -143,21 +137,51 @@ public class S_CMovement : MonoBehaviour
 
         if(onGround){
             rB.gravityScale = fallGravityAir;
+            coyoteCount = 1;
         }
         else if(!isJumping)
         {
-            rB.gravityScale = fallGravityLand;
+            if (validCoyote)
+            {
+                rB.gravityScale = fallGravityAir;
+            }
+            else
+            {
+                rB.gravityScale = fallGravityLand;
+            }
+            
+            Coyote();
         }
     }
-
-    public void Jump(){
-        if(onGround)
-        {
+    public void Jump()
+    {
+            bufferCounter = 0f;
             rB.velocity = new Vector2(rB.velocity.x, 0) * Time.deltaTime;
-            rB.AddForce(transform.up * jumpHeight);
-        }      
+            rB.AddForce(transform.up * jumpHeight);   
     }
-
+    public void Coyote()
+    {
+        coyoteCount -= 10 * Time.deltaTime;
+        if (coyoteCount > 0)
+        {
+            validCoyote = true;
+        }
+        else 
+        {
+            validCoyote = false;
+        }
+    }
+    public void Buffer() 
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            bufferCounter = bufferTime;
+        }
+        else
+        {
+            bufferCounter -= Time.deltaTime;
+        }
+    }
     void OnCollisionStay2D(Collision2D col) 
     {
         if (col.collider != null)
@@ -179,12 +203,33 @@ public class S_CMovement : MonoBehaviour
     {
         onGround = false;
     }
-
-    void Debugs() 
+    void Debugs()
     {
-        if (isJumping) 
+        if (isJumping)
         {
             Debug.Log(transform.position.y);
         }
+    }
+    public void Animations()
+    {
+        //Basic Animations
+        if (direction != 0)
+        {
+            animatorCharacter.SetBool("Static",false);
+            animatorCharacter.SetBool("Walk",true);
+            if(direction < 0)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+        else
+        {
+            animatorCharacter.SetBool("Walk",false);
+            animatorCharacter.SetBool("Static",true);
+        }  
     }
 }
