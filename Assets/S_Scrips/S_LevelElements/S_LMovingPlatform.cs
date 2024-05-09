6d4026ade60788c5108cfd32666cc227e9f49d86
Aberche <73166328+Aberche@users.noreply.github.com>
2024-05-09 17:18:49 +0200

    LV2 SteticFinish

--- Assets/S_Scrips/S_LevelElements/S_LMovingPlatform.cs
+++ Assets/S_Scrips/S_LevelElements/S_LMovingPlatform.cs
@@ -7,29 +7,42 @@ public class S_LMovingPlatform : MonoBehaviour
     public Transform posA, posB;
     public float speed;
     Vector3 targetPos;
+    public int StopTime;
+    public int timeToMove;
 
 
     private void Start()
     {
         targetPos = posB.position;
+        timeToMove = StopTime;
     }         
 
     private void Update()
     {
-
+        timeToMove--;
         if(Vector2.Distance(transform.position, posA.position) < 0.05f)
-         {
-            targetPos = posB.position;
-         }   
-                                                                         
-
-        if(Vector2.Distance(transform.position, posB.position) < 0.05f)
-        
         {
+            if(timeToMove < 0 && targetPos != posB.position)
+            {
+                timeToMove = StopTime;
+            }
+            targetPos = posB.position;
+        }   
+                                                                         
+        if(Vector2.Distance(transform.position, posB.position) < 0.05f)
+        {
+            if(timeToMove < 0 && targetPos != posA.position)
+            {
+                timeToMove = StopTime;
+            }
             targetPos = posA.position;
         }
-
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        
+        if(timeToMove < 0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
